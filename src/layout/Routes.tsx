52982c8c30613f823361d7c16905cebee81ab60f
Tomas Kuruc <tomkokuruc@gmail.com>
2022-07-29 11:35:20 +0200

    add routes & privateRoute

---
+++ src/layout/Routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const Routes = (props: Props) => {
+    return (
+        <div className="Routes">
+
+        </div>
+    );
+};
+
+export default Routes;
